fac21ce92ac30f9a1ac2037efedad7f5ce4e28ac
TAGSBM <agardiner@tagsbm.com>
2015-07-28 14:57:22 +1000

    v8 of accounting module
    v8 of accounting module

---
+++ tag_au_accounting/__init__.py
@@ -0,0 +1,1 @@
+import account_fstr, accounts_reports_tag_v8, base_partner_account_changes_tag_v8, company_related_changes_tag_v8, decimal_precision, invoice_open_form_view_tag_v8, l10n_openerp_au, l10n_TAG_account, account_tax_code_tag_v8
---
+++ tag_au_accounting/__openerp__.py
@@ -0,0 +1,14 @@
+
+{
+    'name': 'TAG Australian Accounting',
+    'version': '1.0',
+    'depends': ['account'],
+    'category': 'Localisation/Account Charts',
+    'description': """Localisation module for AU based companies.
+Recommended for SME businesses - categorised as Small to Medium Sized Enterprise (SME) 1-250 employees.  
+Contains: list of 4 digit accounts, chart of accounts, account types, AU tax codes.""",
+    'author': 'TAG Small Biz Community',
+    'website': 'http://www.smallbiz.community',
+    'installable': 'True'
+}
+
